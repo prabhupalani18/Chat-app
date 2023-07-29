2013be1e5f16b84aa3e4b20447d6d95ffb36698e
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-07-29 20:27:29 +0530

    Initial folder structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port ${PORT}`)
+})
