081f449c3a7b63b7f91bcd2a9ae6b064a3697f16
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-07-31 00:47:55 +0530

    Applied scrolling logic

--- src/index.js
+++ src/index.js
@@ -31,6 +31,10 @@ io.on("connection", (socket)=>{
         socket.join(user.room)
         socket.emit('message', generateMessage("Admin",'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage("Admin", `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
@@ -66,6 +70,10 @@ io.on("connection", (socket)=>{
         if(user)
         {
             io.emit('message', generateMessage("Admin", `${user.username} has left`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 })
